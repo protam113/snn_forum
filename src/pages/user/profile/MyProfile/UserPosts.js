21ce5aa4d86a327ae0324390942d898a220d88e9
protam113 <64359566+protam113@users.noreply.github.com>
2024-08-27 11:56:47 +0700

    27/08 test 2

--- src/pages/user/profile/MyProfile/UserPosts.js
+++ src/pages/user/profile/MyProfile/UserPosts.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FaRegCommentAlt, FaEdit, FaTrashAlt, FaFlag } from "react-icons/fa";
 import { BsThreeDots } from "react-icons/bs";
@@ -20,9 +20,8 @@ const Userblogs = () => {
   const { likedBlogs, handleDeleteBlog, handleLike } = useBlog();
   const { theme } = useTheme();
   const navigate = useNavigate();
-  const [activeMenu, setActiveMenu] = React.useState(null);
-  const [expandedBlogs, setExpandedBlogs] = React.useState({});
-
+  const [activeMenu, setActiveMenu] = useState(null);
+  const [expandedBlogId, setExpandedBlogId] = useState(null);
   const handleMenuClick = (blogId) => {
     setActiveMenu((prev) => (prev === blogId ? null : blogId));
   };
@@ -36,10 +35,7 @@ const Userblogs = () => {
   };
 
   const handleToggleExpand = (blogId) => {
-    setExpandedBlogs((prev) => ({
-      ...prev,
-      [blogId]: !prev[blogId],
-    }));
+    setExpandedBlogId((prev) => (prev === blogId ? null : blogId));
   };
 
   const handleDeleteClick = async (blogId) => {
@@ -107,212 +103,224 @@ const Userblogs = () => {
   const posts = userBlogs.results || [];
 
   return (
-    <>
+    <div className="post-list">
       {posts.length === 0 ? (
         <p>Không có bài viết nào để hiển thị.</p>
       ) : (
-        posts.map((blog) => (
-          <Block
-            className={`p-4 rounded-lg border mt-4 ${
-              theme === "dark"
-                ? "border-custom-zinc bg-gray-800"
-                : "border-gray-300 bg-white"
-            } shadow-sm`}
-          >
-            <div className="flex items-center mb-4">
-              {blog.user.profile_image ? (
-                <img
-                  src={blog.user.profile_image}
-                  alt="avatar"
-                  className={`size-12 rounded-full ${
-                    theme === "dark" ? "border-white" : "border-black"
-                  }`}
-                />
-              ) : (
-                <MdPerson
-                  className={`text-4xl ${
-                    theme === "dark" ? "text-gray-400" : "text-gray-600"
-                  }`}
-                />
-              )}
-              <div className="ml-2">
-                <h1
-                  className={`text-base font-bold leading-tight ${
-                    theme === "dark" ? "text-white" : "text-black"
-                  }`}
-                >
-                  <span>{blog.user.username}</span>
-                </h1>
-                <p
-                  className={`text-gray-500 text-14 ${
-                    theme === "dark" ? "text-gray-400" : "text-gray-500"
-                  }`}
-                >
-                  {formatDate(blog.created_date)}
-                </p>
-              </div>
-              <div className="ml-auto relative">
-                <BsThreeDots
-                  className={`text-2xl cursor-pointer ${
-                    theme === "dark"
-                      ? "text-gray-300 hover:text-gray-200"
-                      : "text-gray-500 hover:text-gray-700"
-                  }`}
-                  onClick={() => handleMenuClick(blog.id)}
-                />
-                {activeMenu === blog.id && (
-                  <div
-                    ref={menuRef} // Apply ref here
-                    className={`absolute right-0 mt-2 w-48 ${
-                      theme === "dark"
-                        ? "bg-gray-700 border-gray-600"
-                        : "bg-zinc-200 border-gray-300"
-                    } border shadow-lg rounded-lg z-10`}
+        posts.map((blog) => {
+          const isExpanded = expandedBlogId === blog.id;
+
+          return (
+            <Block
+              key={blog.id}
+              className={`p-4 rounded-lg border mt-4 ${
+                theme === "dark"
+                  ? "border-custom-zinc bg-gray-800"
+                  : "border-gray-300 bg-white"
+              } shadow-sm`}
+            >
+              <div className="flex items-center mb-4">
+                {blog.user.profile_image ? (
+                  <img
+                    src={blog.user.profile_image}
+                    alt="avatar"
+                    className={`size-12 rounded-full ${
+                      theme === "dark" ? "border-white" : "border-black"
+                    }`}
+                  />
+                ) : (
+                  <MdPerson
+                    className={`text-4xl ${
+                      theme === "dark" ? "text-gray-400" : "text-gray-600"
+                    }`}
+                  />
+                )}
+                <div className="ml-2">
+                  <h1
+                    className={`text-base font-bold leading-tight ${
+                      theme === "dark" ? "text-white" : "text-black"
+                    }`}
                   >
-                    <ul
-                      className={`text-black-300 ${
-                        theme === "dark" ? "text-gray-200" : "text-black"
-                      }`}
+                    <span>{blog.user.username}</span>
+                  </h1>
+                  <p
+                    className={`text-gray-500 text-14 ${
+                      theme === "dark" ? "text-gray-400" : "text-gray-500"
+                    }`}
+                  >
+                    {formatDate(blog.created_date)}
+                  </p>
+                </div>
+                <div className="ml-auto relative">
+                  <BsThreeDots
+                    className={`text-2xl cursor-pointer ${
+                      theme === "dark"
+                        ? "text-gray-300 hover:text-gray-200"
+                        : "text-gray-500 hover:text-gray-700"
+                    }`}
+                    onClick={() => handleMenuClick(blog.id)}
+                  />
+                  {activeMenu === blog.id && (
+                    <div
+                      ref={menuRef}
+                      className={`absolute right-0 mt-2 w-48 ${
+                        theme === "dark"
+                          ? "bg-gray-700 border-gray-600"
+                          : "bg-zinc-200 border-gray-300"
+                      } border shadow-lg rounded-lg z-10`}
                     >
-                      <li
-                        className="px-4 py-2 hover:bg-gray-200 hover:text-black cursor-pointer flex items-center"
-                        onClick={() => handleEditClick(blog.id)}
+                      <ul
+                        className={`text-black-300 ${
+                          theme === "dark" ? "text-gray-200" : "text-black"
+                        }`}
                       >
-                        <FaEdit
-                          className={`mr-2 ${
-                            theme === "dark" ? "text-gray-400" : "text-gray-400"
-                          }`}
-                        />
-                        Chỉnh sửa
-                      </li>
-                      <li
-                        className="px-4 py-2 hover:bg-gray-200 hover:text-black cursor-pointer flex items-center"
-                        onClick={() => handleDeleteClick(blog.id)}
-                      >
-                        <FaTrashAlt
-                          className={`mr-2 ${
-                            theme === "dark" ? "text-gray-400" : "text-gray-400"
-                          }`}
-                        />
-                        Xóa
-                      </li>
-                      <li className="px-4 py-2 hover:bg-gray-200 hover:text-black cursor-pointer flex items-center">
-                        <FaFlag
-                          className={`mr-2 ${
-                            theme === "dark" ? "text-gray-400" : "text-gray-400"
-                          }`}
-                        />
-                        Báo cáo
-                      </li>
-                    </ul>
+                        <li
+                          className="px-4 py-2 hover:bg-gray-200 hover:text-black cursor-pointer flex items-center"
+                          onClick={() => handleEditClick(blog.id)}
+                        >
+                          <FaEdit
+                            className={`mr-2 ${
+                              theme === "dark"
+                                ? "text-gray-400"
+                                : "text-gray-400"
+                            }`}
+                          />
+                          Chỉnh sửa
+                        </li>
+                        <li
+                          className="px-4 py-2 hover:bg-gray-200 hover:text-black cursor-pointer flex items-center"
+                          onClick={() => handleDeleteClick(blog.id)}
+                        >
+                          <FaTrashAlt
+                            className={`mr-2 ${
+                              theme === "dark"
+                                ? "text-gray-400"
+                                : "text-gray-400"
+                            }`}
+                          />
+                          Xóa
+                        </li>
+                        <li className="px-4 py-2 hover:bg-gray-200 hover:text-black cursor-pointer flex items-center">
+                          <FaFlag
+                            className={`mr-2 ${
+                              theme === "dark"
+                                ? "text-gray-400"
+                                : "text-gray-400"
+                            }`}
+                          />
+                          Báo cáo
+                        </li>
+                      </ul>
+                    </div>
+                  )}
+                </div>
+              </div>
+              <p
+                onClick={() => handleBlogClick(blog.id)}
+                className={`mb-8 text-15 cursor-pointer font-semibold ${
+                  theme === "dark" ? "text-gray-300" : "text-black"
+                }`}
+              >
+                {blog.content}
+              </p>
+              <div
+                onClick={() => handleBlogClick(blog.id)}
+                className={`mb-8 text-14 ${
+                  theme === "dark" ? "text-gray-300" : "text-black"
+                } ${isExpanded ? "" : "line-clamp-3"}`}
+                dangerouslySetInnerHTML={{
+                  __html: isExpanded
+                    ? blog.description
+                    : `${blog.description.slice(0, 300)}${
+                        blog.description.length > 300 ? "..." : ""
+                      }`,
+                }}
+              />
+              {!isExpanded && blog.description.length > 300 && (
+                <p
+                  className="text-red-500 cursor-pointer"
+                  onClick={() => handleToggleExpand(blog.id)}
+                >
+                  Thêm
+                </p>
+              )}
+              {isExpanded && (
+                <p
+                  className="text-red-500 cursor-pointer"
+                  onClick={() => handleToggleExpand(blog.id)}
+                >
+                  Ẩn Bớt
+                </p>
+              )}
+              <div className="flex flex-col items-center p-4">
+                {blog.media.length > 0 && (
+                  <div
+                    className={`grid gap-4 ${
+                      blog.media.length === 1
+                        ? "grid-cols-1"
+                        : blog.media.length === 2
+                        ? "grid-cols-2"
+                        : blog.media.length === 3
+                        ? "grid-cols-3"
+                        : "grid-cols-2"
+                    }`}
+                  >
+                    {blog.media.map((media) => renderMedia(media))}
                   </div>
                 )}
               </div>
-            </div>
-            <p
-              onClick={() => handleBlogClick(blog.id)}
-              className={`mb-8 text-15 cursor-pointer font-semibold ${
-                theme === "dark" ? "text-gray-300" : "text-black"
-              }`}
-            >
-              {blog.content}
-            </p>
-            <p
-              onClick={() => handleBlogClick(blog.id)}
-              className={`mb-8 text-14 ${
-                theme === "dark" ? "text-gray-300" : "text-black"
-              } ${expandedBlogs[blog.id] ? "" : "line-clamp-3"}`}
-              dangerouslySetInnerHTML={{
-                __html: expandedBlogs[blog.id]
-                  ? blog.description
-                  : `${blog.description.slice(0, 300)}${
-                      blog.description.length > 300 ? "..." : ""
-                    }`,
-              }}
-            />
-            {!expandedBlogs[blog.id] && blog.description.length > 300 && (
-              <p
-                className="text-red-500 cursor-pointer"
-                onClick={() => handleToggleExpand(blog.id)}
-              >
-                Xem thêm
-              </p>
-            )}
-            {expandedBlogs[blog.id] && (
-              <p
-                className="text-red-500 cursor-pointer"
-                onClick={() => handleToggleExpand(blog.id)}
-              >
-                Xem ít hơn
-              </p>
-            )}
-            <div className="flex flex-col items-center p-4">
-              {/* Kiểm tra và hiển thị các phương tiện truyền thông nếu có */}
-              {blog.media.length > 0 && (
-                <div
-                  className={`grid gap-4 ${
-                    blog.media.length === 1
-                      ? "grid-cols-1"
-                      : blog.media.length === 2
-                      ? "grid-cols-2"
-                      : blog.media.length === 3
-                      ? "grid-cols-3"
-                      : "grid-cols-2"
+              <hr
+                className={`my-4 ${
+                  theme === "dark" ? "border-gray-600" : "border-gray-300"
+                }`}
+              />
+              <div className="relative">
+                <p
+                  className={`text-gray-500 text-sm cursor-pointer ${
+                    theme === "dark" ? "text-gray-400" : "text-gray-500"
                   }`}
                 >
-                  {blog.media.map((media) => renderMedia(media, theme))}
-                </div>
-              )}
-            </div>
-            <hr
-              className={`my-4 ${
-                theme === "dark" ? "border-gray-600" : "border-gray-300"
-              }`}
-            />
-            <div className="relative">
-              <p
-                className={`text-gray-500 text-sm cursor-pointer ${
-                  theme === "dark" ? "text-gray-400" : "text-gray-500"
+                  {blog.likes_count} lượt thích • {blog.comment_count} bình luận
+                </p>
+              </div>
+              <hr
+                className={`my-2 ${
+                  theme === "dark" ? "border-gray-600" : "border-zinc-900"
                 }`}
-              >
-                {blog.likes_count} lượt thích • {blog.comments_count} bình luận
-              </p>
-            </div>
-            <hr
-              className={`my-2 ${
-                theme === "dark" ? "border-gray-600" : "border-zinc-900"
-              }`}
-            />
-            <div className="flex items-center justify-between mb-4">
-              <div className="flex items-center gap-4">
-                <Likeblog
-                  blogId={blog.id}
-                  liked={likedBlogs[blog.id] || false}
-                  onLike={handleLike}
-                />
-                <FaRegCommentAlt
-                  className={`text-2xl cursor-pointer ${
-                    theme === "dark" ? "text-gray-300" : "text-gray-500"
-                  }`}
-                  onClick={() => handleBlogClick(blog.id)}
-                />
-                {/* <BiRepost
+              />
+              <div className="flex items-center justify-between mb-4">
+                <div className="flex items-center gap-4">
+                  <Likeblog
+                    blogId={blog.id}
+                    liked={likedBlogs[blog.id] || false}
+                    onLike={handleLike}
+                  />
+                  <FaRegCommentAlt
+                    className={`text-2xl cursor-pointer ${
+                      theme === "dark" ? "text-gray-300" : "text-gray-500"
+                    }`}
+                    onClick={() => handleBlogClick(blog.id)}
+                  />
+                  <IoShareSocialOutline
+                    className={`text-2xl cursor-pointer ${
+                      theme === "dark" ? "text-gray-300" : "text-gray-500"
+                    }`}
+                    onClick={() => handleCopyLink(blog.id)}
+                  />
+                </div>
+                <div className="flex items-center gap-4">
+                  {/* <BsBookmark
                   className={`text-2xl cursor-pointer ${
                     theme === "dark" ? "text-gray-300" : "text-gray-500"
                   }`}
                 /> */}
+                </div>
               </div>
-              <IoShareSocialOutline
-                className={`text-2xl cursor-pointer ${
-                  theme === "dark" ? "text-gray-300" : "text-gray-500"
-                }`}
-                onClick={() => handleCopyLink(blog.id)}
-              />
-            </div>
-          </Block>
-        ))
+            </Block>
+          );
+        })
       )}
-    </>
+    </div>
   );
 };
 
